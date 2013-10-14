2e7301d13942ee026cdfd11ce8bffb8bc3e2003b
trucktor <aarepin@gmail.com>
2013-10-14 19:42:56 +0300

    Create 2.rb

---
+++ 2.rb
@@ -0,0 +1,7 @@
+puts "What is your name?"
+name = gets.chomp
+puts "What is your patronymic?"
+patro = gets.chomp
+puts "What is your surname?"
+surname = gets.chomp
+puts 'Hello, '+name+' '+patro+' '+surname+'!'
